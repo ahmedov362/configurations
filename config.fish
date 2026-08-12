e9f744419359cffe048157ba954d7c31f4a82eae
Shamil Akhmedov <ahmedovc2422@gmail.com>
2026-08-12 22:51:20 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,16 @@
+if status is-interactive
+    # Отключаем приветствие Fish
+    set fish_greeting
+
+    # Скрываем вывод ошибок батареи (для Starship/Waybar)
+    set -g theme_display_battery no
+
+end
+function set_wall
+    awww img $argv
+    wallust run $argv
+end
+
 function wt
     python ~/Projects/weather/weather.py
 end
@@ -17,11 +30,7 @@ alias lg='lazygit'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias update='sudo dnf upgrade --refresh'
-alias install='sudo dnf install'
-alias remove='sudo dnf remove'
-alias clean='sudo dnf autoremove'
-alias search='dnf search'
+alias install='sudo pacman -S'
 alias myip='curl ifconfig.me'
 alias pinggoogle='ping -c 5 8.8.8.8'
 alias fetch='fastfetch'
@@ -30,14 +39,27 @@ alias pyversion='python3 --version'
 alias cls='clear'
 alias usage='du -sh * | sort -h'
 alias space='df -h'
-alias cava='cava -p ~/.config/cava/themes/noctalia'
-alias nano='nvim'
+alias cava='cava'
 alias ef='nvim ~/.config/fish/config.fish'
 alias sf='source ~/.config/fish/config.fish'
-alias theme='qs -c ~/.config/scripts/theme.py' # не полностью видел
 alias projects='cd ~/Projects/'
 alias lessons='cd ~/Projects/lessons'
 alias aliases='fish ~/Projects/lessons/main.fish '
-alias sfish 'source ~/.config/fish/config.fish'
-alias clr 'clear'
-alias ls 'lsd'
+alias sfish='source ~/.config/fish/config.fish'
+alias clr=clear
+alias ls=lsd
+alias lsend="/home/akmedovshamil/Downloads/LocalSend-*.AppImage --enable-features=UseOzonePlatform --ozone-platform=wayland"
+function fastfetch_smart
+    if test $COLUMNS -lt 110
+        # Если окно узкое — подгружаем конфиг с маленьким логотипом
+        fastfetch -c ~/.config/fastfetch/small.jsonc
+    else
+        # Если окно широкое — подгружаем конфиг с большим логотипом
+        fastfetch -c ~/.config/fastfetch/big.jsonc
+    end
+end
+
+# Создаем быстрый алиас, чтобы при вводе команды "fastfetch" работал наш умный скрипт
+alias ff=fastfetch_smart
+fastfetch_smart
+
